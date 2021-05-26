84347cf9eabc0143fc38de209fc4848b53039be9
adityagaikwad-28 <ag733385@gmail.com>
2021-05-26 11:53:58 +0530

    Home commit

---
+++ app/src/main/java/com/project/homepagemodule/MainActivity.java
@@ -0,0 +1,26 @@
+package com.project.homepagemodule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv, tv1, tv2;
+    ImageView img, img1, img2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
+        tv1 = (TextView) findViewById(R.id.tv1);
+        tv2 = (TextView) findViewById(R.id.tv2);
+        img = (ImageView) findViewById(R.id.img);
+        img1 = (ImageView) findViewById(R.id.img1);
+        img2 = (ImageView) findViewById(R.id.img2);
+    }
+}
